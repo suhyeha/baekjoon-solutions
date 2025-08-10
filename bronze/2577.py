f07b344d0cd14670be839e20411887202b44ff73
suhyeha <a35086293@gmail.com>
2025-08-10 23:18:19 +0900

    숫자의 개수

--- bronze/2577.py
+++ bronze/2577.py
@@ -4,15 +4,13 @@ c=int(input())
 result=a*b*c
 result_str=str(result)
 
-count=[0]*10  #0부터 9까지 카운트 저장 리스트
+count=[0]*10  
 
 for ch in result_str:
     digit=int(ch)
-    count[digit]+=1 #해당하는 숫자 카운트
+    count[digit]+=1
 
 for i in count:
     print(i)
 
-
-#문자열로 변환해서 각 자리 숫자 카운트 하기
 
